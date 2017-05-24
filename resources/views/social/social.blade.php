7d1d7a6d3e0d7f5189bd4a1f8844afacfd489cc1
cbasil <cbasil@126.com>
2017-05-24 18:00:38 +0800

    add ico

--- resources/views/social/social.blade.php
+++ resources/views/social/social.blade.php
@@ -73,9 +73,10 @@
             </div>
             <div class="col-md-4" >
                 <div class="row" style="border-top: 1px solid #c9caca;"> 
-                    <div class="case-img" style="background-image: url('../img/social02.jpg');min-height: 1013px;">
-                        <p style="font-size: 12.5px;padding:35px 45px;">
-                           立于2014年10月20日，本基金会是由本公司创立、接受员工自愿入会的互助互济性组织，对特困员工实施救助，面向社会开展爱心捐助等活动的爱心救助、非营利性的企业公益性组织，配合社会基本医疗保险的，及时扶助患重大疾病患者家庭,因工残疾导致家庭突发性经济困难，秉承“一方有难多方支援”的传统美德，增强企业凝聚力和向心力。
+                    <div class="case-img" style="background-image: url('../img/social02.jpg');min-height: 1013px;padding:45px 45px;">
+                    <h3 style="color: #c3222c">深圳市传承爱心基金会</h3>
+                        <p style="font-size: 12.5px;padding-top: 10px;line-height: 21px;">
+                           成立于2014年10月20日，本基金会是由本公司创立、接受员工自愿入会的互助互济性组织，对特困员工实施救助，面向社会开展爱心捐助等活动的爱心救助、非营利性的企业公益性组织，配合社会基本医疗保险的，及时扶助患重大疾病患者家庭,因工残疾导致家庭突发性经济困难，秉承“一方有难多方支援”的传统美德，增强企业凝聚力和向心力。
                         </p>
                     </div>
                 </div>
