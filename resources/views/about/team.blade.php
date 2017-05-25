37d319303ed6b8d4a6502c0c67df5efa6a05cee7
cbasil <cbasil@126.com>
2017-05-25 10:52:59 +0800

    social menu pages

--- resources/views/about/team.blade.php
+++ resources/views/about/team.blade.php
@@ -7,10 +7,10 @@
 @stop
 @section('text-content')
     <div class="clearfix">
-        <div style="float: left;width: 17.5%">
+        <div class="c-img-fr">
             <img src="../img/li.png" alt="" class="img-responsive" >
         </div>
-        <div  style="float:left;background-color: #f2f2f2;padding:15.5px 15px;width: 82.5%;font-size: 10.5px;">        
+        <div  class="c-img-fl">        
             <h3>李新超  总经理</h3>
             <p>
                 全面负责传承基金的战略规划及运营管理等。近20年金融行业从业经历，先后任职于友邦、平安及海康人寿等金融机构，专业从事不动产投资及公司运营管理，完成近百亿私募基金募集及投资管理，拥有丰富的金融操盘经验和金融机构管理运营经验，对宏观经济运行规律、房地产发展趋势及不动产投资领域资本市场运作均有深入研究。</p>
@@ -18,10 +18,10 @@
     </div>
 
      <div class="clearfix" style="padding-top: 20px;">
-        <div style="float: left;width: 17.5%">
+        <div class="c-img-fr">
             <img src="../img/tao.png" alt="" class="img-responsive" >
         </div>
-        <div  style="float:left;background-color: #f2f2f2;padding:15.5px 15px;width: 82.5%;font-size: 10.5px;">        
+        <div  class="c-img-fl">        
             <h3>李新超  总经理</h3>
             <p>
                 全面负责传承基金的战略规划及运营管理等。近20年金融行业从业经历，先后任职于友邦、平安及海康人寿等金融机构，专业从事不动产投资及公司运营管理，完成近百亿私募基金募集及投资管理，拥有丰富的金融操盘经验和金融机构管理运营经验，对宏观经济运行规律、房地产发展趋势及不动产投资领域资本市场运作均有深入研究。</p>
@@ -29,10 +29,10 @@
     </div>  
 
      <div class="clearfix" style="padding-top: 20px;">
-        <div style="float: left;width: 17.5%">
+        <div class="c-img-fr">
             <img src="../img/zhang.png" alt="" class="img-responsive" >
         </div>
-        <div  style="float:left;background-color: #f2f2f2;padding:15.5px 15px;width: 82.5%;font-size: 10.5px;">        
+        <div  class="c-img-fl">        
             <h3>李新超  总经理</h3>
             <p>
                 全面负责传承基金的战略规划及运营管理等。近20年金融行业从业经历，先后任职于友邦、平安及海康人寿等金融机构，专业从事不动产投资及公司运营管理，完成近百亿私募基金募集及投资管理，拥有丰富的金融操盘经验和金融机构管理运营经验，对宏观经济运行规律、房地产发展趋势及不动产投资领域资本市场运作均有深入研究。</p>
@@ -40,10 +40,10 @@
     </div>  
 
      <div class="clearfix" style="padding-top: 20px;">
-        <div style="float: left;width: 17.5%">
+        <div class="c-img-fr">
             <img src="../img/gao.png" alt="" class="img-responsive" >
         </div>
-        <div  style="float:left;background-color: #f2f2f2;padding:15.5px 15px;width: 82.5%;font-size: 10.5px;">        
+        <div  class="c-img-fl">        
             <h3>李新超  总经理</h3>
             <p>
                 全面负责传承基金的战略规划及运营管理等。近20年金融行业从业经历，先后任职于友邦、平安及海康人寿等金融机构，专业从事不动产投资及公司运营管理，完成近百亿私募基金募集及投资管理，拥有丰富的金融操盘经验和金融机构管理运营经验，对宏观经济运行规律、房地产发展趋势及不动产投资领域资本市场运作均有深入研究。</p>
--- resources/views/social/social.blade.php
+++ resources/views/social/social.blade.php
@@ -31,9 +31,7 @@
     @section('banner')
     <div class="c-subtitle c-fonts-uppercase c-fonts-bold c-bg-img-center" >
       <img src="../img/social.jpg" alt="Image-1" width="100%" height="auto">
-    </div>
-   
-            
+    </div>       
     @stop
     @section('content')
     <div class="c-content-box  c-bg-white" style="padding-top: 45px;">
@@ -41,11 +39,11 @@
             <div class="row">
                 <div class="col-md-6">
                    	<div class="row c-menu-left">
-                     	  <div class="c-menu-left-img">
+                     	<div class="c-menu-left-img">
                             <span style="background-image: url(../img/home.png);"></span>
-                     	      <a href="{{route('company')}}">首页</a>
+                     	    <a href="{{url('/')}}">首页</a>
                         </div>
-                     	  <div class="c-menu-left-img">
+                     	<div class="c-menu-left-img">
                             <span style="background-image: url(../img/arrows.png);"></span>
                             <a href="">社会责任</a>
                         </div>
@@ -53,9 +51,9 @@
                 </div>
                 <div class="col-md-6">
                    	<div class="row">
-                     		<div class="c-menu-right">
+                     	<div class="c-menu-right">
                      	
-                     		</div>
+                     	</div>
                    	</div>
                 </div>
             </div>
@@ -66,15 +64,63 @@
             <div class="col-md-8" >
                 <div class="row" style="border-top: 1px solid #c9caca;">   
                     <div class="text-content">
-                    	@section('text-content')
-                    	@show
+                    	<ul class="social-ul">
+                            <li style="padding: 20px 0;">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="../img/social03.jpg" alt="" class="img-responsive">
+                                    </div>
+                                    <div class="com-md-9">
+                                        <h3 class="c-social-date">2017-4-27</h3>
+                                        <a href="" class="c-social-title">
+                                            <h3 >涓滴之水成海洋，颗颗爱心变希望——湖北黄冈助学行捐赠仪式</h3>
+                                        </a>
+                                        <p style="font-size: 12.5px;color: #727171">
+                                            2017年4月26日，深圳市传承爱心基金会携手深圳狮子会爱心人士,前往周畈小学开展湖北黄冈助学行捐赠仪式，参加仪式的深圳狮子会的爱心人士有：深圳狮子会上步服务队创会会长暨同致行（中国）地产顾问集团董事长王波狮兄，
+                                            </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li style="padding: 20px 0;">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="../img/social03.jpg" alt="" class="img-responsive">
+                                    </div>
+                                    <div class="com-md-9">
+                                        <h3 class="c-social-date">2017-4-27</h3>
+                                        <a href="" class="c-social-title">
+                                            <h3 >涓滴之水成海洋，颗颗爱心变希望——湖北黄冈助学行捐赠仪式</h3>
+                                        </a>
+                                        <p style="font-size: 12.5px;color: #727171">
+                                            2017年4月26日，深圳市传承爱心基金会携手深圳狮子会爱心人士,前往周畈小学开展湖北黄冈助学行捐赠仪式，参加仪式的深圳狮子会的爱心人士有：深圳狮子会上步服务队创会会长暨同致行（中国）地产顾问集团董事长王波狮兄，
+                                            </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li style="padding: 20px 0;">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="../img/social03.jpg" alt="" class="img-responsive">
+                                    </div>
+                                    <div class="com-md-9">
+                                        <h3 class="c-social-date">2017-4-27</h3>
+                                        <a href="" class="c-social-title">
+                                            <h3>涓滴之水成海洋，颗颗爱心变希望——湖北黄冈助学行捐赠仪式</h3>
+                                        </a>
+                                        <p style="font-size: 12.5px;color: #727171">
+                                            2017年4月26日，深圳市传承爱心基金会携手深圳狮子会爱心人士,前往周畈小学开展湖北黄冈助学行捐赠仪式，参加仪式的深圳狮子会的爱心人士有：深圳狮子会上步服务队创会会长暨同致行（中国）地产顾问集团董事长王波狮兄，
+                                            </p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
             <div class="col-md-4" >
                 <div class="row" style="border-top: 1px solid #c9caca;"> 
                     <div class="case-img" style="background-image: url('../img/social02.jpg');min-height: 1013px;padding:45px 45px;">
-                    <h3 style="color: #c3222c">深圳市传承爱心基金会</h3>
+                        <h3 style="color: #c3222c">深圳市传承爱心基金会</h3>
                         <p style="font-size: 12.5px;padding-top: 10px;line-height: 21px;">
                            成立于2014年10月20日，本基金会是由本公司创立、接受员工自愿入会的互助互济性组织，对特困员工实施救助，面向社会开展爱心捐助等活动的爱心救助、非营利性的企业公益性组织，配合社会基本医疗保险的，及时扶助患重大疾病患者家庭,因工残疾导致家庭突发性经济困难，秉承“一方有难多方支援”的传统美德，增强企业凝聚力和向心力。
                         </p>
